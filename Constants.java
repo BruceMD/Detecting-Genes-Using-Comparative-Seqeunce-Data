99aef12d6c3ea8fca974f2d48720d993d78a40fc
BruceMD <maxbruce9@gmail.com>
2018-09-18 22:35:10 +0200

    Add Constants Class
    This file contains the stop codons and frame enums

---
+++ Constants.java
@@ -0,0 +1,13 @@
+public class Constants {
+
+	public enum StopCodon {
+		TAG, TAA, TGA
+	}
+	
+	public static final String FRAME_1 = "1";
+	public static final String FRAME_2 = "2";
+	public static final String FRAME_3 = "3";
+	public static final String FRAME_N1 = "-1";
+	public static final String FRAME_N2 = "-2";
+	public static final String FRAME_N3 = "-3";	
+}
