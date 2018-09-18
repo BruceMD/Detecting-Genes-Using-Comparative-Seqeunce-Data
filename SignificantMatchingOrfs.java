f29bab5f5e809c288be16988883497af537f4afc
BruceMD <maxbruce9@gmail.com>
2018-09-18 22:31:36 +0200

    Significant MAtching ORFs class that contain the Array of ORFs

---
+++ SignificantMatchingOrfs.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class SignificantMatchingOrfs {
+	private ArrayList<SignificantOrf> SignificantOrfs = new ArrayList<>();
+
+	public ArrayList<SignificantOrf> getSignificantOrfs() {
+		return SignificantOrfs;
+	}
+
+	public void setSignificantOrfs(ArrayList<SignificantOrf> significantOrfs) {
+		SignificantOrfs = significantOrfs;
+	}
+}
