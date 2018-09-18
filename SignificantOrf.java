8e8c8026f0c2c161e1149721e26dce90dbfdbebe
BruceMD <maxbruce9@gmail.com>
2018-09-18 22:28:42 +0200

    Significant ORF class. Contains start and end positions of each significant ORF

---
+++ SignificantOrf.java
@@ -0,0 +1,17 @@
+public class SignificantOrf {
+	private int start;
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	
+	private int end;
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+}
