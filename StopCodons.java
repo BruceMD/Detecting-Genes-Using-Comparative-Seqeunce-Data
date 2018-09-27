004cc5a0ed5361ac782b3a80994c5000e05beef8
BruceMD <maxbruce9@gmail.com>
2018-09-27 16:31:08 +0200

    Finds all the stop codons

--- StopCodons.java
+++ StopCodons.java
@@ -2,14 +2,36 @@ import java.util.ArrayList;
 
 public class StopCodons {
 	
-	public static void FrameOne() throws Exception {
+	public static ArrayList<ArrayList<ArrayList<Integer>>> AllStopCodons() throws Exception{
+	
+		ArrayList<ArrayList<ArrayList<Integer>>> array = new ArrayList<ArrayList<ArrayList<Integer>>>();
+		
+		array.add(StopCodons.FrameOne());
+		array.add(StopCodons.FrameTwo());
+		array.add(StopCodons.FrameThree());
+		array.add(StopCodons.FrameNOne());
+		array.add(StopCodons.FrameNTwo());
+		array.add(StopCodons.FrameNThree());
+ 		
+/* 		for (int n = 0; n < array.size(); n++) {
+ 			for (int o = 0; o < array.get(n).size(); o++) {
+ 					System.out.println("Frame " + (n+1) + ": " + "Sequence " + (o+1) + " -> " + array.get(n).get(o));
+ 		
+ 			}
+ 		}
+*/ 		
+ 		return array;
+
+	}
+	
+	public static ArrayList<ArrayList<Integer>> FrameOne() throws Exception {	//perfect
 		
 		String tempCodon = new String();		// frame +1
 
 		Integer locationOfTempCodon = null;
 		ArrayList<String> listOfSequences = new ArrayList<String>();
-		ArrayList<ArrayList<Integer>> locationOfAllStopCodons = new ArrayList<ArrayList<Integer>>();	// array within an array
-//		ArrayList<Integer> locationOfStopCodons = new ArrayList<Integer>();
+		ArrayList<ArrayList<Integer>> locationOfAllStopCodonsOne = new ArrayList<ArrayList<Integer>>();	// array within an array
+
 		listOfSequences = ReadFile.Sequence();
 		
 		for (int i = 0; i < listOfSequences.size(); i++) {	// cycle through the items in the Arraylist
@@ -35,7 +57,7 @@ public class StopCodons {
 						 
 					}
 					if(tempCodon.length() >= 3) {												// check if tempCodon is a stop codon
-						if (tempCodon.contains("TAA") || tempCodon.contains("TAG") || tempCodon.contains("TGA") || tempCodon.contains("ATT")) {
+						if (tempCodon.contains("TAA") || tempCodon.contains("TAG") || tempCodon.contains("TGA")) {
 							locationOfStopCodons.add(locationOfTempCodon);						// append index of Stop Codon to array list
 							
 						}																		// frame specific
@@ -50,18 +72,298 @@ public class StopCodons {
 				
 			}		// cycle through the characters in each sequence
 			
-			locationOfAllStopCodons.add(locationOfStopCodons);
+			locationOfAllStopCodonsOne.add(locationOfStopCodons);
 
 		}		// cycle through the items in the Arraylist
-		
-		for (int m = 0; m < locationOfAllStopCodons.size(); m++) {
-			System.out.println(locationOfAllStopCodons.get(m));
-		}
-		
-	
+
+		return locationOfAllStopCodonsOne;
 	}
 
+	public static ArrayList<ArrayList<Integer>> FrameTwo() throws Exception {	// perfect
+			
+			String tempCodon = new String();		// frame +2
+			tempCodon = "NN";
 	
+			Integer locationOfTempCodon = null;
+			ArrayList<String> listOfSequences = new ArrayList<String>();
+			ArrayList<ArrayList<Integer>> locationOfAllStopCodonsTwo = new ArrayList<ArrayList<Integer>>();	// array within an array
+	//		ArrayList<Integer> locationOfStopCodons = new ArrayList<Integer>();
+			listOfSequences = ReadFile.Sequence();
+			
+			for (int i = 0; i < listOfSequences.size(); i++) {	// cycle through the items in the Arraylist
+				
+				ArrayList<Integer> locationOfStopCodons = new ArrayList<Integer>();
+				
+				for (int j = 0; j < listOfSequences.get(i).length(); j++) { 	// cycle through the characters in each sequence
+					
+					if (listOfSequences.get(i).charAt(j) == 'A' || 
+						listOfSequences.get(i).charAt(j) == 'T' || 
+						listOfSequences.get(i).charAt(j) == 'C' || 
+						listOfSequences.get(i).charAt(j) == 'G') 
+					{													// provided the character is only an nucleotide
+	//					System.out.println(tempCodon.length());
+						
+						if (tempCodon.length() == 0 ) {
+							locationOfTempCodon = j;							// temporarily store value of temp codon
 	
+						}
+						
+						if (tempCodon.length() < 3) {								// check if tempCodon is full, if not, add more
+							 tempCodon = tempCodon + listOfSequences.get(i).charAt(j);
+							 
+						}
+						if(tempCodon.length() >= 3) {												// check if tempCodon is a stop codon
+							if (tempCodon.contains("TAA") || tempCodon.contains("TAG") || tempCodon.contains("TGA")) {
+								locationOfStopCodons.add(locationOfTempCodon);						// append index of Stop Codon to array list
+								
+							}																		// frame specific
+							
+							tempCodon = "";										// empty tempCodon to continue cycle	
+	//						locationOfTempCodon = 0;	 														
+			 
+						}													
+					}	
+					
+	
+					
+				}		// cycle through the characters in each sequence
+				
+				locationOfAllStopCodonsTwo.add(locationOfStopCodons);
+	
+			}		// cycle through the items in the Arraylist
+			
+			return locationOfAllStopCodonsTwo;
+			
+		}
+	
+	public static ArrayList<ArrayList<Integer>> FrameThree() throws Exception {	// perfect
+		
+		String tempCodon = new String();		// frame +3
+		tempCodon = "N";
+	
+		Integer locationOfTempCodon = null;
+		ArrayList<String> listOfSequences = new ArrayList<String>();
+		ArrayList<ArrayList<Integer>> locationOfAllStopCodonsThree = new ArrayList<ArrayList<Integer>>();	// array within an array
+	//	ArrayList<Integer> locationOfStopCodons = new ArrayList<Integer>();
+		listOfSequences = ReadFile.Sequence();
+		
+		for (int i = 0; i < listOfSequences.size(); i++) {	// cycle through the items in the Arraylist
+			
+			ArrayList<Integer> locationOfStopCodons = new ArrayList<Integer>();
+			
+			for (int j = 0; j < listOfSequences.get(i).length(); j++) { 	// cycle through the characters in each sequence
+				
+				if (listOfSequences.get(i).charAt(j) == 'A' || 
+					listOfSequences.get(i).charAt(j) == 'T' || 
+					listOfSequences.get(i).charAt(j) == 'C' || 
+					listOfSequences.get(i).charAt(j) == 'G') 
+				{													// provided the character is only an nucleotide
+	//				System.out.println(tempCodon.length());
+					
+					if (tempCodon.length() == 0 ) {
+						locationOfTempCodon = j;							// temporarily store value of temp codon
+	
+					}
+					
+					if (tempCodon.length() < 3) {								// check if tempCodon is full, if not, add more
+						 tempCodon = tempCodon + listOfSequences.get(i).charAt(j);
+						 
+					}
+					if(tempCodon.length() >= 3) {												// check if tempCodon is a stop codon
+						if (tempCodon.contains("TAA") || tempCodon.contains("TAG") || tempCodon.contains("TGA")) {
+							locationOfStopCodons.add(locationOfTempCodon);						// append index of Stop Codon to array list
+							
+						}																		// frame specific
+						
+						tempCodon = "";										// empty tempCodon to continue cycle	
+	//					locationOfTempCodon = 0;	 														
+		 
+					}													
+				}	
+				
+	
+				
+			}		// cycle through the characters in each sequence
+			
+			locationOfAllStopCodonsThree.add(locationOfStopCodons);
+	
+		}		// cycle through the items in the Arraylist
+
+		return locationOfAllStopCodonsThree;
+		
+	}
+	
+	public static ArrayList<ArrayList<Integer>> FrameNOne() throws Exception {
+		
+		String tempCodon = new String();		// frame -1
+	
+		Integer locationOfTempCodon = null;
+		ArrayList<String> listOfSequences = new ArrayList<String>();
+		ArrayList<ArrayList<Integer>> locationOfAllStopCodonsNOne = new ArrayList<ArrayList<Integer>>();	// array within an array
+	//	ArrayList<Integer> locationOfStopCodons = new ArrayList<Integer>();
+		listOfSequences = ReadFile.Sequence();
+		
+		for (int i = 0; i < listOfSequences.size(); i++) {	// cycle through the items in the Arraylist
+			
+			ArrayList<Integer> locationOfStopCodons = new ArrayList<Integer>();
+			
+			for (int j = listOfSequences.get(i).length() - 1; j >= 0 ; j--) { 	// cycle through the characters in each sequence
+				
+				if (listOfSequences.get(i).charAt(j) == 'A' || 
+					listOfSequences.get(i).charAt(j) == 'T' || 
+					listOfSequences.get(i).charAt(j) == 'C' || 
+					listOfSequences.get(i).charAt(j) == 'G') 
+				{													// provided the character is only an nucleotide
+	//				System.out.println(tempCodon.length());
+					
+					if (tempCodon.length() == 0 ) {
+						locationOfTempCodon = j;							// temporarily store value of temp codon
+	
+					}
+					
+					if (tempCodon.length() < 3) {								// check if tempCodon is full, if not, add more
+						 tempCodon = tempCodon + listOfSequences.get(i).charAt(j);
+						 
+					}
+					if(tempCodon.length() >= 3) {												// check if tempCodon is a stop codon
+						if (tempCodon.contains("TAA") || tempCodon.contains("TAG") || tempCodon.contains("TGA")) {
+							locationOfStopCodons.add(locationOfTempCodon);						// append index of Stop Codon to array list
+							
+						}																		// frame specific
+						
+						tempCodon = "";										// empty tempCodon to continue cycle	
+	//					locationOfTempCodon = 0;	 														
+		 
+					}													
+				}	
+				
+	
+				
+			}		// cycle through the characters in each sequence
+			
+			locationOfAllStopCodonsNOne.add(locationOfStopCodons);
+	
+		}		// cycle through the items in the Arraylist
+		
+		return locationOfAllStopCodonsNOne;
+		
+	}
+	
+	public static ArrayList<ArrayList<Integer>> FrameNTwo() throws Exception {
+		
+		String tempCodon = new String();		// frame -2
+		tempCodon = "NN";
+	
+		Integer locationOfTempCodon = null;
+		ArrayList<String> listOfSequences = new ArrayList<String>();
+		ArrayList<ArrayList<Integer>> locationOfAllStopCodonsNTwo = new ArrayList<ArrayList<Integer>>();	// array within an array
+	//	ArrayList<Integer> locationOfStopCodons = new ArrayList<Integer>();
+		listOfSequences = ReadFile.Sequence();
+		
+		for (int i = 0; i < listOfSequences.size(); i++) {	// cycle through the items in the Arraylist
+			
+			ArrayList<Integer> locationOfStopCodons = new ArrayList<Integer>();
+			
+			for (int j = listOfSequences.get(i).length() - 1; j >= 0 ; j--) { 	// cycle through the characters in each sequence
+				
+				if (listOfSequences.get(i).charAt(j) == 'A' || 
+					listOfSequences.get(i).charAt(j) == 'T' || 
+					listOfSequences.get(i).charAt(j) == 'C' || 
+					listOfSequences.get(i).charAt(j) == 'G') 
+				{													// provided the character is only an nucleotide
+	//				System.out.println(tempCodon.length());
+					
+					if (tempCodon.length() == 0 ) {
+						locationOfTempCodon = j;							// temporarily store value of temp codon
+	
+					}
+					
+					if (tempCodon.length() < 3) {								// check if tempCodon is full, if not, add more
+						 tempCodon = tempCodon + listOfSequences.get(i).charAt(j);
+						 
+					}
+					if(tempCodon.length() >= 3) {												// check if tempCodon is a stop codon
+						if (tempCodon.contains("TAA") || tempCodon.contains("TAG") || tempCodon.contains("TGA")) {
+							locationOfStopCodons.add(locationOfTempCodon);						// append index of Stop Codon to array list
+							
+						}																		// frame specific
+						
+						tempCodon = "";										// empty tempCodon to continue cycle	
+	//					locationOfTempCodon = 0;	 														
+		 
+					}													
+				}	
+				
+	
+				
+			}		// cycle through the characters in each sequence
+			
+			locationOfAllStopCodonsNTwo.add(locationOfStopCodons);
+	
+		}		// cycle through the items in the Arraylist
+	
+		return locationOfAllStopCodonsNTwo;
+		
+	}
+	
+	public static ArrayList<ArrayList<Integer>> FrameNThree() throws Exception {
+		
+		String tempCodon = new String();		// frame -3
+		tempCodon = "N";
+	
+		Integer locationOfTempCodon = null;
+		ArrayList<String> listOfSequences = new ArrayList<String>();
+		ArrayList<ArrayList<Integer>> locationOfAllStopCodonsNThree = new ArrayList<ArrayList<Integer>>();	// array within an array
+	//	ArrayList<Integer> locationOfStopCodons = new ArrayList<Integer>();
+		listOfSequences = ReadFile.Sequence();
+		
+		for (int i = 0; i < listOfSequences.size(); i++) {	// cycle through the items in the Arraylist
+			
+			ArrayList<Integer> locationOfStopCodons = new ArrayList<Integer>();
+			
+			for (int j = listOfSequences.get(i).length() - 1; j >= 0 ; j--) { 	// cycle through the characters in each sequence
+				
+				if (listOfSequences.get(i).charAt(j) == 'A' || 
+					listOfSequences.get(i).charAt(j) == 'T' || 
+					listOfSequences.get(i).charAt(j) == 'C' || 
+					listOfSequences.get(i).charAt(j) == 'G') 
+				{													// provided the character is only an nucleotide
+	//				System.out.println(tempCodon.length());
+					
+					if (tempCodon.length() == 0 ) {
+						locationOfTempCodon = j;							// temporarily store value of temp codon
+	
+					}
+					
+					if (tempCodon.length() < 3) {								// check if tempCodon is full, if not, add more
+						 tempCodon = tempCodon + listOfSequences.get(i).charAt(j);
+						 
+					}
+					if(tempCodon.length() >= 3) {												// check if tempCodon is a stop codon
+						if (tempCodon.contains("TAA") || tempCodon.contains("TAG") || tempCodon.contains("TGA")) {
+							locationOfStopCodons.add(locationOfTempCodon);						// append index of Stop Codon to array list
+							
+						}																		// frame specific
+						
+						tempCodon = "";										// empty tempCodon to continue cycle	
+	//					locationOfTempCodon = 0;	 														
+		 
+					}													
+				}	
+				
+	
+				
+			}		// cycle through the characters in each sequence
+			
+			locationOfAllStopCodonsNThree.add(locationOfStopCodons);
+	
+		}		// cycle through the items in the Array list
+	
+		return locationOfAllStopCodonsNThree;
+		
+	}
+	
+		
+		
 }
 	
