40415f068f2590e36eb7d4f4b3fec4b24039020a
BruceMD <maxbruce9@gmail.com>
2018-09-27 16:31:52 +0200

    Changed the file that was being read

--- ReadFile.java
+++ ReadFile.java
@@ -3,10 +3,9 @@ import java.io.*;
 
 public class ReadFile {
 	
-	public static void main(String[] args) throws Exception {
+	public static ArrayList<String> Sequence() throws Exception {
 	
-		File file = new File("C:\\Users\\maxbr\\Desktop\\Bioinformatics\\Sample Seqs\\Flu.fasta");
-		File fileName = new File("C:\\Users\\maxbr\\Desktop\\Bioinformatics\\Sample Seqs\\ExampleSeq.fas");
+		File file = new File("C:\\Users\\maxbr\\Desktop\\Bioinformatics\\Sample Seqs\\Example2(A-J-cons-kal153).fas");
 		BufferedReader buffReader = new BufferedReader(new FileReader(file));
 	
 		String tempLine = null;
@@ -20,27 +19,38 @@ public class ReadFile {
 					Sequences.add(" ");
 				}
 				else {
-					Sequences.add(tempString + "\n");
+					Sequences.add("\n" + tempString);
 					tempString = "";
 				}
 			}
 			else if (!tempLine.contains(">")) {
-				System.out.println(tempLine);
+//				System.out.println(tempLine);
 				tempString = tempString + tempLine.trim();
 			}	
 		}
 		Sequences.add(tempString);
-	
-		BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+		tempString = "";
+		
+		buffReader.close();
+		
+/*		File fileName = new File("C:\\Users\\maxbr\\Desktop\\Bioinformatics\\Sample Seqs\\ExampleSeq1.fas");
+
+ 		BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
 		
 		for (int i = 1; i < Sequences.size(); i++) {
 			writer.write(Sequences.get(i));
 		}
-		
-		System.out.println(Sequences);
-		System.out.println(Sequences.size());
+*/		
+//		System.out.println(Sequences);
+//		System.out.println(Sequences.size());
 	
-		buffReader.close();
-		writer.close();
+		
+//		writer.close();
+		
+		return Sequences;
+		
 	}
+	
+	
+	
 }
