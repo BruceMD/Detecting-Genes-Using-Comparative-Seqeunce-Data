878457793c4317cb3ae074f268813d3f95d82f6f
BruceMD <maxbruce9@gmail.com>
2018-10-03 09:47:18 +0200

    Running different classes

--- OrfFinder.java
+++ OrfFinder.java
@@ -1,30 +1,14 @@
-import java.io.File;
-import java.io.FileReader;
-import java.io.BufferedReader;
+// import java.io.File;
+// import java.io.FileReader;
+// import java.io.BufferedReader;
 
 public class OrfFinder {
 
-	public static void main(String[] args) throws Exception {
-
-		File file = new File("C:\\Users\\maxbr\\Desktop\\Bioinformatics\\Sample Seqs\\Example1(PotySeqs).fas");
-		
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		
-		String st;
-		
-		while ((st = br.readLine()) != null) {
-			if (st.contains(">")) {
-				System.out.println("\n" + st);
-			}
-			else {
-				String tempLine = st.trim();
-				System.out.print(tempLine);
-			}			
+		public static void main(String[] args) throws Exception {
+	
+//			StopCodons.AllStopCodons();
+//			TempSizeOrfs.Difference();
+			SignificantOrf.AllOrfs();
+			
 		}
-		
-		br.close();
-				
-				
-	}
-
 }
