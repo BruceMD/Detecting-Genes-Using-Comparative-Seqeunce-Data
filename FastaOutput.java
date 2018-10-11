65c9bc5e2ffc64ac3753a6c416acb01ca0ce57f7
Bruce Dickie <38924242+BruceMD@users.noreply.github.com>
2018-10-11 12:58:17 +0200

    Update FastaOutput.java

--- FastaOutput.java
+++ FastaOutput.java
@@ -15,13 +15,20 @@ public class FastaOutput {
 		for (int i = 0; i < OrfsArray.size(); i++) {
 			for (int j = 0; j < OrfsArray.get(i).size(); j++) {
 				
-				File fileName = new File("C:\\Users\\maxbr\\Desktop\\Bioinformatics\\Sample Seqs\\ExampleFMDV\\Orf" + (count +1) + ".fas");
-				BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+				File fileNameFub = new File("C:\\Users\\maxbr\\Desktop\\Bioinformatics\\Sample Seqs\\ExampleFMDV\\FubOrf" + (count +1) + ".fas");
+				BufferedWriter writerFub = new BufferedWriter(new FileWriter(fileNameFub));
+				
+				File fileNameRax = new File("C:\\Users\\maxbr\\Desktop\\Bioinformatics\\Sample Seqs\\ExampleFMDV\\RaxOrf" + (count +1) + ".fas");
+				BufferedWriter writerRax = new BufferedWriter(new FileWriter(fileNameRax));
 			
 				for (int k = 0; k < OrfsArray.get(i).get(j).size(); k++) {
+					
 					String orf = OrfsArray.get(i).get(j).get(k);
+					
+					writerRax.write(orf + "\n");
+					
 					orf = orf.replace("-", "");
-					writer.write(orf + "\n");
+					writerFub.write(orf + "\n");
 				}
 				
 				count = count + 1;
